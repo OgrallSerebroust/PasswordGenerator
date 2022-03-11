e2e173fc8e2a06508ff6e860d39e27b096a4bcb6
Ograll <stefplotva@gmail.com>
2022-03-11 12:51:21 +0300

    MainWindow Added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+from sys import argv, exit
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindowPart(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("PasswordGenerator2.0")
+
+
+if __name__ == "__main__":
+    application = QApplication(argv)
+    program = MainWindowPart()
+    program.show()
+    exit(application.exec_())
